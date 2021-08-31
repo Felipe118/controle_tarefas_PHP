ba5a9c6f32b3a9871d6069b633c9f49e31ab0540
Felipe118 <lipebezerraa@gmail.com>
2021-08-31 20:08:42 -0300

    Adicionando a tradução para o envio de emails

--- routes/web.php
+++ routes/web.php
@@ -22,11 +22,11 @@ Route::get('/', function () {
 
 Auth::routes(); 
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('verified');
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('verified');
 Route::resource('tarefa', 'App\Http\Controllers\TarefaController');
 Route::get('mensagem-teste', function(){
    // return new MensagemTesteMail();
